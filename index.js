cffe7bf3351fdb129d398c24ae528a3b34a7014a
David Banks <amoebae@gmail.com>
2019-09-27 10:30:10 +0100

    example of use of $onChanges hook

--- index.js
+++ index.js
@@ -4,15 +4,41 @@ import angular from 'angular';
 // declare the application
 const phonecatApplication = angular.module('phonecatApplication', []);
 
+phonecatApplication.component('phoneItem', {
+    bindings: {
+        item: '<',
+        blurb: '<'
+    },
+    templateUrl: 'phone-item.tmpl',
+    controller() {
+        this.$onChanges = function(changes) {
+            console.log("changes in bound variables");
+            console.log("changes were: %o", changes);
+
+            console.log(changes.blurb.currentValue);
+            console.log(changes.blurb.previousValue);
+        };
+    }
+});
+
 phonecatApplication.component('phoneList', {
     templateUrl: 'phone-list.tmpl',
     controller() {
         this.phones = [
             {'name': 'Motorola Timeport',
-             'snippet': "Alan's current phone"},
+             'snippet': "Alan's current phone",
+             'age': 20},
             {'name': 'Nokia 3310',
-             'snippet': "A timeless classic"},
+             'snippet': "A timeless classic",
+             'age': 19},
         ];
+
+        this.orderProp = 'age';
+        this.blurb = "This space intentionally left blank";
+
+        this.modifyBlurb = function() {
+            this.blurb = "Some new blurb";
+        };
     }
 });
 
